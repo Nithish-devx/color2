42dbb7e29677caa3a1edac3d9c836872403b939c
Nithish-devx <Nithishdhana2004@gmail.com>
2023-02-23 11:36:14 +0530

    First commit

---
+++ src/main/java/com/example/demo/Color2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Color2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Color2Application.class, args);
+	}
+
+}
